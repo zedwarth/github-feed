5516f3d6fc16da1588d95763e7bfd8f721f93ece
zedwarth <zedwarth@gmail.com>
2014-02-20 17:23:18 -0600

    Add gravatar method.

--- lib/github.rb
+++ lib/github.rb
@@ -11,6 +11,11 @@ module GHFeed
 	end
 	class UserInfo
 		def self.gravatar_url(username)
+			url = "https://api.github.com/users/#{username}"
+			response = Unirest.get url, auth:{ :user=>ENV["USERNAME"], :password=>ENV["PASSWORD"] }
+			
+			id = response.body["gravatar_id"]
+			"https://secure.gravatar.com/avatar/#{id}"
 		end
 	end
 end
