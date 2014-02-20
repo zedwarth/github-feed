92f4f6c55b57b555b6de96917cf73f7f2dc5a328
zedwarth <zedwarth@gmail.com>
2014-02-20 15:10:45 -0600

    Add spec file skeleton. Describe GHFeed::repoInfo .get_repo_data Describe GHFeed::UserInfo .get_user_data

---
+++ spec/ghfeed_spec.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'rspec'
+require_relative '../lib/github.rb'
+
+# Remember, # is short for instance method, and . is short for class method
+
+describe GHFeed::RepoInfo do
+	describe '.get_repo_data'
+end
+
+describe GHFeed::UserInfo do
+	describe '.get_user_data'
+end
