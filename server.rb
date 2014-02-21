bc58ef2e65fffc866ea2335418c283a8e352bfa3
zedwarth <zedwarth@gmail.com>
2014-02-21 17:21:13 -0600

    Feed working.

--- server.rb
+++ server.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+require_relative 'lib/github'
+get '/' do
+	"Hey, this is a web app"
+end
+
+get '/test' do
+		"#{GHFeed::RepoInfo.get_repo_data("clamstew", "matchsetter")}"
+end
+
 get '/feed/:username/:reponame' do
+	@commits = GHFeed::RepoInfo.get_repo_data(params[:username].to_s, params[:reponame].to_s)
+	@user_info = GHFeed::UserInfo.gravatar_url(params[:username].to_s)
 	erb :feed
 end
+
+
